ba94ff9524fc7dfc6a8fd9e818025ac7208e327c
Аргоков Амаду Иженович <amaduargokov594@gmail.com>
2024-11-20 23:19:05 +0700

    Create Graphics Widget

--- Cirrus/Cirrus.cpp
+++ Cirrus/Cirrus.cpp
@@ -4,7 +4,16 @@ Cirrus::Cirrus(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+    setWindowTitle("Cirrus");
+    scene = new QGraphicsScene();
 }
 
 Cirrus::~Cirrus()
-{}
+{ }
+
+void Cirrus::on_btnClose_clicked() {
+    ui.graphicsView->setScene(scene);
+    scene->setBackgroundBrush(Qt::white);
+    ui.btnClose->close();
+
+}
--- Cirrus/Cirrus.h
+++ Cirrus/Cirrus.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QGraphicsScene>
 #include "ui_Cirrus.h"
 
 class Cirrus : public QMainWindow
@@ -8,9 +9,11 @@ class Cirrus : public QMainWindow
     Q_OBJECT
 
 public:
-    Cirrus(QWidget *parent = nullptr);
+    Cirrus(QWidget* parent = nullptr);
     ~Cirrus();
-
+public slots:
+    void on_btnClose_clicked();
 private:
     Ui::CirrusClass ui;
+    QGraphicsScene *scene;
 };
